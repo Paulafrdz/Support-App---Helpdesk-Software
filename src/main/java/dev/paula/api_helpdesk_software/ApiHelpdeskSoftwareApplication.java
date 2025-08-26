881d495d8f049fafc41f08ea56615cd45339c345
Paula Fernandez Aranda <paulafa8@gmail.com>
2025-08-26 16:10:23 +0200

    first commit

---
+++ src/main/java/dev/paula/api_helpdesk_software/ApiHelpdeskSoftwareApplication.java
@@ -0,0 +1,13 @@
+package dev.paula.api_helpdesk_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHelpdeskSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHelpdeskSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/paula/api_helpdesk_software/ApiHelpdeskSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.paula.api_helpdesk_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHelpdeskSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
