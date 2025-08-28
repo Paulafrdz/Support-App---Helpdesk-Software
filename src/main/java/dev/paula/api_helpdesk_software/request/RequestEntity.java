5dc545ba6f6d22e0cf3b9405753ee49936fbe57a
Paula Fernandez Aranda <paulafa8@gmail.com>
2025-08-28 12:38:18 +0200

    feat: ResquestEntity

---
+++ src/main/java/dev/paula/api_helpdesk_software/request/RequestEntity.java
@@ -0,0 +1,44 @@
+package dev.paula.api_helpdesk_software.request;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "requests")
+public class RequestEntity {
+    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public RequestEntity() {
+    }
+
+    public RequestEntity(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+}
